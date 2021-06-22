b348887ac43af7b061f9a7b1cb25e0432d4c7a3f
rumi0608 <79518215+rumi0608@users.noreply.github.com>
2021-06-22 15:54:38 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,99 @@
+var dog,sadDog,happyDog,garden,washroom, database;
+var foodS,foodStock;
+var fedTime,lastFed,currentTime;
+var feed,addFood;
+var foodObj;
+var gameState,readState;
+
+function preload()
+{
+ dogSad = loadImage("images/dogImg.png");
+ dogHappy = loadImage("images/dogImg1.png");
+}
+
+function setup() {
+   database=firebase.database();
+  createCanvas(400,500);
+
+ 
+
+  foodStock=database.ref('food');
+  foodStock.on("value",readStock);
+
+  fedTime=database.ref('FeedTime');
+  fedTime.on("value",function(data){
+    lastFed=data.val();
+  });
+
+  //read game state from database
+  readState=database.ref('gameState');
+  readState.on("value",function(data){
+    gameState=data.val();
+  });
+   
+  dog=createSprite(200,400,150,150);
+  dog.addImage(sadDog);
+  dog.scale=0.15;
+  
+  feed=createButton("Feed the dog");
+  feed.position(700,95);
+  feed.mousePressed(feedDog);
+
+  addFood=createButton("Add Food");
+  addFood.position(800,95);
+  addFood.mousePressed(addFoods);
+}
+
+function draw() {
+  currentTime=hour();
+  if(currentTime==(lastFed+1)){
+    update("Hungry")
+    foodObj.display();
+   }
+   
+   if(gameState!="Hungry"){
+     feed.hide();
+     addFood.hide();
+     dog.remove();
+   }else{
+    feed.show();
+    addFood.show();
+    dog.addImage("dog,dogSad");
+   }
+ 
+  drawSprites();
+}
+
+//function to read food Stock
+function readStock(data){
+  foodS=data.val();
+   
+}
+
+
+//function to update food stock and last fed time
+function feedDog(){
+  dog.addImage("dog,dogHappy");
+
+  foodObj.updateFoodStock(foodObj.getFoodStock()-1);
+  database.ref('/').update({
+    Food:foodObj.getFoodStock(),
+    FeedTime:hour(),
+    gameState:"Hungry"
+  })
+}
+
+//function to add food in stock
+function addFoods(){
+  foodS++;
+  database.ref('/').update({
+    Food:foodS
+  })
+}
+
+//update gameState
+function update(state){
+  database.ref('/').update({
+    gameState:state
+  })
+}
